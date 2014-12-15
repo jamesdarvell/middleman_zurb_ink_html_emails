eedeed4336d74cbfe34243e92bc502d8ae1af515
James Darvell <jdarvell@sur.co.uk>
2014-12-15 12:23:20 +0100

    Add a helper for rows and refactor

--- helpers/ink_grid_helpers.rb
+++ helpers/ink_grid_helpers.rb
@@ -1,13 +1,22 @@
 module InkGridHelpers
   def ink_container(options = {}, &block)
-    raise 'Cannot have a container without contents!' unless block_given?
+    return_ink_markup('container', options, &block)
+  end
+
+  def ink_row(options = {}, &block)
+    return_ink_markup('row', options, &block)
+  end
+
+  private
+  def return_ink_markup(name, options, &block)
+    raise "Cannot have a #{name} without contents!" unless block_given?
 
     partial(
-      '_partials/_ink/container',
-      :locals => {
-          :content => capture_html(&block),
-          :classes => options[:classes] || []
-      }
+        "_partials/_ink/#{name}",
+        :locals => {
+            :content => capture_html(&block),
+            :classes => options[:classes] || []
+        }
     )
   end
 end
