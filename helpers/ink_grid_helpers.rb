8820a771e6c1f6a248fa984c2ed1297f71b5f8b8
James Darvell <jdarvell@sur.co.uk>
2014-12-15 13:32:23 +0100

    Add column helper and refactor

--- helpers/ink_grid_helpers.rb
+++ helpers/ink_grid_helpers.rb
@@ -7,16 +7,24 @@ module InkGridHelpers
     return_ink_markup('row', options, &block)
   end
 
+  def ink_column(size, options = {}, &block)
+    options[:size] = size # it is always required, hence the required parameter
+    options[:last] ||= false
+
+    return_ink_markup('column', options, &block)
+  end
+
   private
   def return_ink_markup(name, options, &block)
     raise "Cannot have a #{name} without contents!" unless block_given?
 
-    partial(
-        "_partials/_ink/#{name}",
-        :locals => {
-            :content => capture_html(&block),
-            :classes => options[:classes] || []
-        }
-    )
+    partial("_partials/_ink/#{name}", :locals => build_locals_hash(options, &block))
+  end
+
+  def build_locals_hash(options, &block)
+    options[:classes] ||= []
+    options.merge({
+      :content => capture_html(&block)
+    })
   end
 end
