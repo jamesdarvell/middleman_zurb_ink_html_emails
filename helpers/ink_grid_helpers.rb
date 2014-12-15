021074576721cda63da5a8038dd21ee2fd81c5b0
James Darvell <jdarvell@sur.co.uk>
2014-12-15 12:08:07 +0100

    Add ability to include arbitrary classes to container helper

--- helpers/ink_grid_helpers.rb
+++ helpers/ink_grid_helpers.rb
@@ -1,7 +1,13 @@
 module InkGridHelpers
-  def ink_container(&block)
+  def ink_container(options = {}, &block)
     raise 'Cannot have a container without contents!' unless block_given?
 
-    partial('_partials/_ink/container', :locals => {:content => capture_html(&block)})
+    partial(
+      '_partials/_ink/container',
+      :locals => {
+          :content => capture_html(&block),
+          :classes => options[:classes] || []
+      }
+    )
   end
 end
