3fb121545b65f322441af7ef4a73b5937a1f05ab
James Darvell <jdarvell@sur.co.uk>
2014-12-16 21:02:27 +0100

    Add centered content helper and example

--- lib/html_emails/helpers/ink_content_helpers.rb
+++ lib/html_emails/helpers/ink_content_helpers.rb
@@ -5,3 +5,7 @@ end
 def ink_panel(options = {}, &block)
   return_ink_markup('panel', options, &block)
 end
+
+def ink_center(options = {}, &block)
+  return_ink_markup('center', options, &block)
+end
