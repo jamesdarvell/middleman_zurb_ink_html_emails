aaa575653b174afa5d7effa94f114e26001dfb49
James Darvell <jdarvell@sur.co.uk>
2014-12-16 21:26:47 +0100

    Alter behaviour of ink content helpers as content can be centered and also a panel

--- lib/html_emails/helpers/ink_content_helpers.rb
+++ lib/html_emails/helpers/ink_content_helpers.rb
@@ -2,10 +2,15 @@ def ink_boilerplate(&block)
   return_ink_markup('boilerplate', {}, &block)
 end
 
+def ink_content(types, options = {}, &block)
+  options[:content_types] = types
+  return_ink_markup('content', options, &block)
+end
+
 def ink_panel(options = {}, &block)
-  return_ink_markup('panel', options, &block)
+  ink_content(['panel'], options, &block)
 end
 
 def ink_center(options = {}, &block)
-  return_ink_markup('center', options, &block)
+  ink_content(['center'], options, &block)
 end
