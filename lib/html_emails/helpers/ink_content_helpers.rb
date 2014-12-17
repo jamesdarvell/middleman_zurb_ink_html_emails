6b8089ab176e0e9c7e9af25caf5d762fe62eccee
James Darvell <jdarvell@sur.co.uk>
2014-12-17 10:56:03 +0100

    Added ink text padding helpers

--- lib/html_emails/helpers/ink_content_helpers.rb
+++ lib/html_emails/helpers/ink_content_helpers.rb
@@ -2,7 +2,7 @@ def ink_boilerplate(&block)
   return_ink_markup('boilerplate', {}, &block)
 end
 
-def ink_content(types, options = {}, &block)
+def ink_content(types = [], options = {}, &block)
   options[:content_types] = types
   return_ink_markup('content', options, &block)
 end
@@ -13,4 +13,12 @@ end
 
 def ink_center(options = {}, &block)
   ink_content(['center'], options, &block)
+end
+
+def ink_left_text_pad(options = {}, &block)
+  ink_content(['left-text-pad'], options, &block)
+end
+
+def ink_right_text_pad(options = {}, &block)
+  ink_content(['right-text-pad'], options, &block)
 end
