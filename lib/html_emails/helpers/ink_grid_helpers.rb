704a4f3604fca5daa18eef8bba246790db177e1d
James Darvell <jdarvell@sur.co.uk>
2014-12-17 13:23:53 +0100

    Added support for sub-grid columns

--- lib/html_emails/helpers/ink_grid_helpers.rb
+++ lib/html_emails/helpers/ink_grid_helpers.rb
@@ -18,3 +18,11 @@ end
 def ink_full_width_row(options = {}, &block)
   return_ink_markup('full_width_row', options, &block)
 end
+
+def ink_sub_column(size, options = {}, &block)
+  throw HtmlEmails.SIZE_WRONG unless HtmlEmails.size_valid?(size)
+  options[:size] = HtmlEmails.size_as_word(size)
+  options[:last] ||= false
+
+  return_ink_markup('sub_column', options, &block)
+end
