e57a2ca0371009ff69880aedab05fbe7a53e0bdc
James Darvell <jdarvell@sur.co.uk>
2014-12-17 21:51:02 +0100

    Add support for button sizes

--- lib/html_emails/helpers/ink_button_helpers.rb
+++ lib/html_emails/helpers/ink_button_helpers.rb
@@ -1,4 +1,17 @@
+BUTTON_STYLES = %w{button small-button tiny-button medium-button large-button}
+
 def ink_button(options = {}, &block)
   options[:href] ||= '#'
+
+  options[:classes] ||= []
+  options[:classes] << correct_button_class(options)
+
   return_ink_markup('button', options, &block)
+end
+
+def correct_button_class(options)
+  # if there is a correct button class, add nothing
+  return nil if (options[:classes] & BUTTON_STYLES).size > 0
+
+  options[:size] ? "#{options[:size]}-button" : "button"
 end
