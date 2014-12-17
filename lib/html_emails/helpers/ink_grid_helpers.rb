814c182844d5ab861ca5a97830b25972d6b2b73a
James Darvell <jdarvell@sur.co.uk>
2014-12-17 12:00:43 +0100

    Add full-width-row helper and partial

--- lib/html_emails/helpers/ink_grid_helpers.rb
+++ lib/html_emails/helpers/ink_grid_helpers.rb
@@ -14,3 +14,7 @@ def ink_column(size, options = {}, &block)
 
   return_ink_markup('column', options, &block)
 end
+
+def ink_full_width_row(options = {}, &block)
+  return_ink_markup('full_width_row', options, &block)
+end
