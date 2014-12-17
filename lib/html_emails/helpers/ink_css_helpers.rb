cd1d29c741648932b53e7b543634d368c9a7ffa4
James Darvell <jdarvell@sur.co.uk>
2014-12-17 11:23:06 +0100

    Add a helper to allow user to add css styles into the inlined css

--- lib/html_emails/helpers/ink_css_helpers.rb
+++ lib/html_emails/helpers/ink_css_helpers.rb
@@ -1,7 +1,13 @@
 def inline_ink_css
-  ink_css =  minimize_css(IO.read(File.dirname(__FILE__) + '/../css/ink.css'))
+  ink_css =  minimize_css(IO.read(File.dirname(__FILE__) + '/../css/ink.css')) + @user_css
 
   "<style type='text/css'>#{ink_css}</style>"
+end
+
+
+def ink_css(&block)
+  @user_css = (@user_css || '') + capture_html(&block)
+  ''
 end
 
 def minimize_css(css)
