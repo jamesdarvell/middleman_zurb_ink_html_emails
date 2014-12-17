ef026c819f421eea407bcba40903d0f28f50fcfb
James Darvell <jdarvell@sur.co.uk>
2014-12-17 11:26:17 +0100

    Quick fix to allow documents without user added css, also

--- lib/html_emails/helpers/ink_css_helpers.rb
+++ lib/html_emails/helpers/ink_css_helpers.rb
@@ -1,9 +1,9 @@
 def inline_ink_css
-  ink_css =  minimize_css(IO.read(File.dirname(__FILE__) + '/../css/ink.css')) + @user_css
+  ink_css =   minimize_css(IO.read(File.dirname(__FILE__) + '/../css/ink.css'))
+  ink_css +=  @user_css if @user_css
 
   "<style type='text/css'>#{ink_css}</style>"
 end
-
 
 def ink_css(&block)
   @user_css = (@user_css || '') + capture_html(&block)
