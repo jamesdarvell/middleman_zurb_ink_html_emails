62a729b0b5055b5eb958d12ea4eceb8f0e6c7ff4
James Darvell <jdarvell@sur.co.uk>
2014-12-17 20:20:04 +0100

    Add support for visibility classes

--- lib/html_emails/helpers/ink_basic_helpers.rb
+++ lib/html_emails/helpers/ink_basic_helpers.rb
@@ -6,6 +6,8 @@ end
 
 def build_locals_hash(options, &block)
   options[:classes] ||= []
+  options[:classes] << 'hide-for-small' if options[:visibility] == :hide_for_small
+  options[:classes] << 'show-for-small' if options[:visibility] == :show_for_small
   options.merge({
     :content => capture_html(&block)
   })
