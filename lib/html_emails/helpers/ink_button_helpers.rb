640ff1e6773bca0cd63c9548c56fb67164bf53b4
James Darvell <jdarvell@sur.co.uk>
2014-12-18 10:30:16 +0100

    Add button color support

--- lib/html_emails/helpers/ink_button_helpers.rb
+++ lib/html_emails/helpers/ink_button_helpers.rb
@@ -1,4 +1,5 @@
-BUTTON_STYLES = %w{button small-button tiny-button medium-button large-button}
+BUTTON_TYPES = %w{button small-button tiny-button medium-button large-button}
+BUTTON_COLORS = %w{primary secondary alert success}
 
 def ink_button(options = {}, &block)
   check_options_valid(options)
@@ -7,13 +8,24 @@ def ink_button(options = {}, &block)
 
   options[:classes] ||= []
   options[:classes] << correct_button_class(options)
+  options[:classes] << correct_button_color_class(options)
 
   return_ink_markup('button', options, &block)
 end
 
 def correct_button_class(options)
   # if there is a correct button class, add nothing
-  return nil if (options[:classes] & BUTTON_STYLES).size > 0
+  return nil if (options[:classes] & BUTTON_TYPES).size > 0
 
-  options[:size] ? "#{options[:size]}-button" : "button"
+  options[:size] ? "#{options[:size].to_s}-button" : "button"
+end
+
+def correct_button_color_class(options)
+  # if there is a correct button color class, add nothing
+  return nil if (options[:classes] & BUTTON_COLORS).size > 0
+  return nil unless options[:color]
+
+  raise 'Invalid button color' unless BUTTON_COLORS.include? options[:color].to_s
+
+  options[:color].to_s
 end
