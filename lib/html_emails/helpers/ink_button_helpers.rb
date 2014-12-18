503fffd728ecb73490fddbd2612999e7ab617615
James Darvell <jdarvell@sur.co.uk>
2014-12-18 11:03:30 +0100

    Add button radius support

--- lib/html_emails/helpers/ink_button_helpers.rb
+++ lib/html_emails/helpers/ink_button_helpers.rb
@@ -1,5 +1,6 @@
 BUTTON_TYPES = %w{button small-button tiny-button medium-button large-button}
 BUTTON_COLORS = %w{primary secondary alert success}
+BUTTON_RADII = %w{radius round}
 
 def ink_button(options = {}, &block)
   check_options_valid(options)
@@ -9,6 +10,7 @@ def ink_button(options = {}, &block)
   options[:classes] ||= []
   options[:classes] << correct_button_class(options)
   options[:classes] << correct_button_color_class(options)
+  options[:classes] << correct_radius_class(options)
 
   return_ink_markup('button', options, &block)
 end
@@ -28,4 +30,14 @@ def correct_button_color_class(options)
   raise 'Invalid button color' unless BUTTON_COLORS.include? options[:color].to_s
 
   options[:color].to_s
+end
+
+def correct_radius_class(options)
+  # if there is a correct radius class, add nothing
+  return nil if (options[:classes] & BUTTON_RADII).size > 0
+  return nil unless options[:radius]
+
+  raise 'Invalid button radius' unless BUTTON_RADII.include? options[:radius].to_s
+
+  options[:radius].to_s
 end
