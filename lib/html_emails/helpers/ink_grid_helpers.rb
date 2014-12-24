ff6b0877f372c61a85064bac571fb2676562dd64
James Darvell <jdarvell@sur.co.uk>
2014-12-24 13:19:00 +0100

    Add helpers for stackable columns (multiple columns within same wrapper - they are stacked on top of each other) and for a single cell table, used to hold content (I call this a section)

--- lib/html_emails/helpers/ink_grid_helpers.rb
+++ lib/html_emails/helpers/ink_grid_helpers.rb
@@ -28,6 +28,23 @@ def ink_section(options = {}, &block)
   return_ink_markup('section', options, &block)
 end
 
+def ink_wrapper(options = {}, &block)
+  check_options_valid(options)
+
+  options[:last] ||= false
+
+  return_ink_markup('wrapper', options, &block)
+end
+
+def ink_stacked_column(size, options = {}, &block)
+  throw HtmlEmails.SIZE_WRONG unless HtmlEmails.size_valid?(size)
+  check_options_valid(options)
+
+  options[:size] = HtmlEmails.size_as_word(size)
+
+  return_ink_markup('stacked_column', options, &block)
+end
+
 def ink_sub_column(size, options = {}, &block)
   throw HtmlEmails.SIZE_WRONG unless HtmlEmails.size_valid?(size)
   check_options_valid(options)
