d39009a3470b9424d13a19ebfd34162e05fd502d
James Darvell <jdarvell@sur.co.uk>
2014-12-25 03:02:06 +0100

    The ColumnSizes module needs to be included in the zurb ink module

--- lib/html_emails/zurb_ink.rb
+++ lib/html_emails/zurb_ink.rb
@@ -1,3 +1,4 @@
+require_relative 'helpers/column_sizes'
 require_relative 'helpers/ink_basic_helpers'
 require_relative 'helpers/ink_css_helpers'
 require_relative 'helpers/ink_content_helpers'
@@ -6,20 +7,7 @@ require_relative 'helpers/ink_button_helpers'
 require_relative 'helpers/ink_image_helpers'
 
 module ZurbInk
-  SIZE_WRONG = 'Size must be an integer between 1 and 12 (or a number in the same range written as a word)'
-  SIZE_RANGE = (1..12)
-  SIZE_RANGE_AS_WORDS = %w{one two three four five six seven eight nine ten eleven twelve}
-  INK_COLUMN_WIDTHS = [0, 30, 80, 130, 180, 230, 280, 330, 380, 430, 480, 530, 580]
-
-  def size_valid?(size)
-    (SIZE_RANGE === size) || SIZE_RANGE_AS_WORDS.include?(size)
-  end
-
-  def size_as_word(size)
-    return size unless SIZE_RANGE === size
-
-    return SIZE_RANGE_AS_WORDS[size - 1]
-  end
+  include ColumnSizes
 
   # zurb ink specific helpers are defined in these specific modules. They are added to the HtmlEmail extension when it
   # extends the ZurbInk module.
